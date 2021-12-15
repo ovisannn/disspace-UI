e4d43c4018a16057013f03bf6ab2824aebd52cdb
ovisannn <ovi.sanjaya11@gmail.com>
2021-12-15 14:55:40 +0700

    Update tailwind.config.js
    updating color pallate to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,19 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors:{
+      lightOrange : '#FFC5A1',
+      orange : '#F8A978',
+      lightTeal : '#BADFDB',
+      lightYellow : '#FCF9EA',
+      lightGray : '#F8F8F8',
+      gray : '#EEEEEE',
+      grayTxt : '#989898',
+      white : '#FFFFFF',
+      black : '#000000',
+      green : '#A5E289',
+      red : '#E28989',
+    },
   },
   plugins: [],
 }

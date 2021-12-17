7824cca077966ca29c1187378f694b4c4b61ee08
ovisannn <ovi.sanjaya11@gmail.com>
2021-12-17 09:43:23 +0700

    about category card done
    done

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import AboutCategories from '../components/AboutCategories'
 import styles from '../styles/Home.module.css'
+import GameHeader from '../img/gameHeader.jpg'
+
+const categories ={
+  header : GameHeader,
+  tag : '#Gaming',
+  description : 'A topic for (almost) anything related to games - video games, board games, card games, etc. (but not sports).',
+}
 
 export default function Home() {
   return (
-    <div className='text-orange'>
-      tes
-    </div>
+    <>
+      <AboutCategories category={categories} />
+      <div className='text-orange'>
+        tes
+      </div>
+    </>
   )
 }

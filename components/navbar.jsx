75aea5ed534075a37c90faddd820328668c9d922
ovisannn <ovi.sanjaya11@gmail.com>
2021-12-17 07:53:56 +0700

    adding Logged-in navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,6 +1,18 @@
+import Link from 'next/link'
 import Image from 'next/image'
 import logo_img from '../img/disspace.png'
 import searchIcon from '../img/searchicon.png'
+import { BellIcon, ChatAltIcon } from '@heroicons/react/outline'
+import NewThreadIcon from '../img/newThread.svg'
+import dummyAvatar from '../img/dummy avatar.jpg'
+
+
+const dummyUser = {
+    id : 18293710,
+    full_name : 'mingun',
+    username : 'jhonwkwkk',
+    avatar : dummyAvatar,
+  }
 
 const LoginButton = () =>{
     return (
@@ -26,7 +38,7 @@ const SearchBar = () => {
     return(
         <div className='flex'>
             <button className='border-y-2 border-l-2 border-orange px-2 pt-1 rounded-l'>
-                <Image src={searchIcon} />
+                <Image src={searchIcon} alt='' />
             </button>
             <input type="search" className='w-96 h-8 border-l-0 rounded-l-none border-2 border-orange rounded focus:w-[771px] transition-all ease-linear outline-none px-2 placeholder:text-orange font-extralight' placeholder='Search' />
         </div>
@@ -42,21 +54,49 @@ const NotUserUI = () => {
     )
 } 
 
-const UserUI = () =>{
+
+const UserUI = ( { userProps } ) =>{
+    const avatar_style = {
+        overflow : 'hidden'
+    }
+
     return(
         <>
-            tes
+            <div className="flex h-12 w-32 justify-between content-center mx-5 py-2">
+                <button className='h-8 w-8 boreder-2 mt-[2px]'>
+                    <Image src={NewThreadIcon} width={28} height={28} alt='' />
+                </button>
+                <button className="h-8 w-8 boreder-2">
+                    <ChatAltIcon className='text-orange'/>
+                </button>
+                <button className="h-8 w-8 boreder-2">
+                    <BellIcon className='text-orange' />
+                </button>
+            </div>
+
+            <button className='grid grid-flow-col justify-between content-center h-12 w-36 border-2 border-orange rounded-lg px-2 hover:text-white hover:bg-orange hover:rounded-none transition-all ease-linear'>
+                <div className="h-8 w-8 rounded-full" style={avatar_style}>
+                    <Image src={userProps.avatar} layout='fixed' width={32} height={32} alt='' />
+                </div>
+                <div className="my-1 font-extralight">
+                    {userProps.full_name}
+                </div>
+            </button>
         </>
     )
 }
 
 const Navbar = () => {
-    let isLoggedIn = false
+    let isLoggedIn = true
     return (
-        <div className=" flex flex-row justify-between w-full md:h-16 shadow-md bg-white shadow-md px-16">
+        <div className=" flex flex-row justify-between w-full md:h-16 bg-white shadow-md px-16">
             {/* logo */}
             <div>
-                <Image src={logo_img} height={63} width={153} />
+                <Link href='/'>
+                    <a>
+                        <Image src={logo_img} height={63} width={153} alt=''/>
+                    </a>
+                </Link>
             </div>
             {/* search bar component */}
             <div className='grid content-center w-6/12 flex justify-center'>
@@ -67,7 +107,7 @@ const Navbar = () => {
                 <div className='flex'>
                     {
                         isLoggedIn
-                        ?<UserUI />
+                        ?<UserUI userProps={ dummyUser } />
                         :<NotUserUI />
                     }
                 </div>

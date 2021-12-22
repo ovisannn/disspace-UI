14021e16b2958574a1f858c54a79e1a9e4407a36
ovisannn <ovi.sanjaya11@gmail.com>
2021-12-22 23:13:00 +0700

    Create LeaderBoards.jsx

---
+++ components/LeaderBoards.jsx
@@ -0,0 +1,36 @@
+const UserButton = ({user, rangking}) =>{
+    return (
+        <div className="flex flex-col md:flex-row md:justify-between bg-white my-1 h-auto w-full shadow rounded-md px-5 py-2">
+            <div>
+                {rangking}
+            </div>
+            <div>
+                {user.username}
+            </div>
+            <div>
+                <button className="bg-orange text-white rounded px-2 font-light">
+                    follow
+                </button>
+            </div>
+        </div>
+    )
+}
+
+const LeaderBoards = ({ topUsers }) => {
+    return (
+        <div className="w-80 h-auto grid grid-flow-row border-2 border-lightGray shadow-sm rounded">
+        <div className="h-20 w-full relative rounded-t text-white font-light text-2xl text-center align-middle py-5 bg-orange">
+            LEADERBOARD
+        </div>
+        <div className="w-full px-5 font-light bg-lightGray">
+            {
+                topUsers.map((user, index) => (
+                    <UserButton key={user.username} user={user} rangking={index+1} />
+                ))
+            }
+        </div>
+    </div>
+    )
+}
+
+export default LeaderBoards

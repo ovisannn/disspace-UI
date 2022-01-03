45203371ece0db89e7fed6884bfe4054e7aa8b11
ovisannn <ovi.sanjaya11@gmail.com>
2022-01-03 17:34:32 +0700

    Update ThreadSelector.jsx
    update dikit

--- components/ThreadSelector.jsx
+++ components/ThreadSelector.jsx
@@ -12,7 +12,7 @@ export default function ThreadSelector() {
   const [selected, setSelected] = useState(option[0])
 
   return (
-    <div className="w-40 top-16">
+    <div className="w-44 top-16">
       <Listbox value={selected} onChange={setSelected}>
         <div className="relative mt-1">
           <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">

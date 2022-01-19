c4958887f4856d24b667b8808680b9abba5e3729
ovisannn <ovi.sanjaya11@gmail.com>
2022-01-19 15:45:39 +0700

    login handle-done
    working on front end

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path:false };
+
+    return config;
+  },
   reactStrictMode: true,  
   images: {
     domains: ["randomuser.me", "images.unsplash.com", "static.wikia.nocookie.net"],
--- pages/api/endpoints.js
+++
@@ -1,3 +0,0 @@
-exports.EndPoints ={
-    getAllthreads : 'http://localhost:8080/v1/threads'
-}

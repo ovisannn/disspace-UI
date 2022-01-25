b90576c3bb5bddce59e76a84f5d79f56b005e0c9
ovisannn <ovi.sanjaya11@gmail.com>
2022-01-25 16:48:09 +0700

    Update NavbarV2.js

--- components/NavbarV2.js
+++ components/NavbarV2.js
@@ -136,7 +136,7 @@ const UserProfile = ({ user }) =>{
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                     >
-                    <Menu.Items className="absolute right-0 w-32 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute w-32 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <div className="px-1 py-1 ">
                         <Menu.Item>
                             {({ active }) => (
